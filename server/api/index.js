6d516a087808976f0ae68890210d134985c7f72f
Joseph <polanco.joseph1@gmail.com>
2022-03-24 13:16:24 -0400

    commited

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
+const serveStatic = require('serve-static');
 module.exports = app;
 
 app.listen(port, () => {
@@ -8,6 +10,14 @@ app.listen(port, () => {
 })
 
 const router = require('./routes');
+
+console.log(__dirname)
+
+app.use('/', serveStatic ( path.join(__dirname, '../index.html' ) ) );
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
 
 app.use(express.json());
 app.use('/api', router);
