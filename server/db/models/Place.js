c8f3e4de892e0058762c30c184a556b074c74f21
Joseph <polanco.joseph1@gmail.com>
2022-03-24 16:40:27 -0400

    commited

--- server/db/models/Place.js
+++ server/db/models/Place.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+console.log(process.env.MONGOLAB_URI, "Process ENV!!!")
+
 mongoose.connect(process.env.MONGOLAB_URI || `mongodb://127.0.0.1:27017/api-project`, { useNewURLParser: true, useUnifiedTopology: true });
 
 let Place = mongoose.model("Place", new mongoose.Schema({
